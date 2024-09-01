fdf5e07190e838cb6681e657a8ed56f0b12e57ef
Yaroslav Honchar <yaroslav.honchar.1@gmail.com>
2024-09-01 16:26:18 +0300

    refactor: category routes

--- app/api/(routes)/[storeId]/categories/[categoryId]/route.ts
+++ app/api/(routes)/[storeId]/categories/[categoryId]/route.ts
@@ -1,91 +1,93 @@
-import { ObjectId } from "bson"
 import type { IPropsWithStoreidCategoryidParam } from "@/types/pages-props.interface"
 import prismadb from "@/lib/prismadb"
+import { authGuard } from "@/app/api/lib/auth-guard"
+import { exceptionFilter } from "@/app/api/lib/exception-filter"
+import { IDValidator } from "@/app/api/lib/id-validator"
 import { auth } from "@clerk/nextjs/server"
 import type { NextRequest } from "next/server"
 import { NextResponse } from "next/server"
 
 // Send a category of a store
-export async function GET(_req: NextRequest, { params: { storeId, categoryId } }: IPropsWithStoreidCategoryidParam) {
-  if (!ObjectId.isValid(storeId) || !ObjectId.isValid(categoryId)) {
-    return new NextResponse("Invalid store or category id", { status: 400 })
-  }
+export const GET = exceptionFilter(
+  "CATEGORY",
+  "GET",
+  IDValidator<IPropsWithStoreidCategoryidParam>(
+    async (_req: NextRequest, { params: { storeId, categoryId } }: IPropsWithStoreidCategoryidParam) => {
+      const category = await prismadb.category.findUnique({
+        where: { storeId, id: categoryId },
+      })
 
-  try {
-    const category = await prismadb.category.findUnique({
-      where: { storeId, id: categoryId },
-    })
-
-    return new NextResponse(JSON.stringify(category), { status: 200 })
-  } catch (error) {
-    console.log("[CATEGORY_GET]", error)
-    return new NextResponse("Internal Server ErrorDisplay", { status: 500 })
-  }
-}
+      return new NextResponse(JSON.stringify(category), { status: 200 })
+    },
+  ),
+)
 
 // Update a category of a store
-export async function PATCH(req: NextRequest, { params: { storeId, categoryId } }: IPropsWithStoreidCategoryidParam) {
-  if (!ObjectId.isValid(storeId) || !ObjectId.isValid(categoryId)) {
-    return new NextResponse("Invalid store or category id", { status: 400 })
-  }
+export const PATCH = exceptionFilter(
+  "CATEGORY",
+  "PATCH",
+  authGuard(
+    IDValidator<IPropsWithStoreidCategoryidParam>(
+      async (req: NextRequest, { params: { storeId, categoryId } }: IPropsWithStoreidCategoryidParam) => {
+        const userId = auth().userId as string
 
-  try {
-    const { userId } = auth()
-    if (!userId) {
-      return new NextResponse("Unauthenticated", { status: 401 })
-    }
+        const storeByUserId = await prismadb.store.findFirst({
+          where: {
+            id: storeId,
+            userId,
+          },
+        })
 
-    const { name, billboardId } = await req.json()
-    if (!name) {
-      return new NextResponse("Name is required", { status: 400 })
-    }
+        if (!storeByUserId) {
+          return new NextResponse("Unauthorized", { status: 403 })
+        }
 
-    if (!billboardId) {
-      return new NextResponse("Billboard id is required", { status: 400 })
-    }
+        const { name, billboardId } = await req.json()
+        if (!name) {
+          return new NextResponse("Name is required", { status: 400 })
+        }
 
-    const storeByUserId = await prismadb.store.findFirst({
-      where: {
-        id: storeId,
-        userId,
+        if (!billboardId) {
+          return new NextResponse("Billboard id is required", { status: 400 })
+        }
+
+        const category = await prismadb.category.update({
+          where: { id: categoryId, storeId },
+          data: { name, billboardId },
+        })
+
+        return new NextResponse(JSON.stringify(category), { status: 200 })
       },
-    })
-
-    if (!storeByUserId) {
-      return new NextResponse("Unauthorized", { status: 403 })
-    }
-
-    const category = await prismadb.category.update({
-      where: { id: categoryId, storeId },
-      data: { name, billboardId },
-    })
-
-    return new NextResponse(JSON.stringify(category), { status: 200 })
-  } catch (error) {
-    console.log("[CATEGORY_PATCH]", error)
-    return new NextResponse("Internal Server ErrorDisplay", { status: 500 })
-  }
-}
+    ),
+  ),
+)
 
 // Delete a category of a store
-export async function DELETE(_req: NextRequest, { params: { storeId, categoryId } }: IPropsWithStoreidCategoryidParam) {
-  if (!ObjectId.isValid(storeId) || !ObjectId.isValid(categoryId)) {
-    return new NextResponse("Invalid store or category id", { status: 400 })
-  }
+export const DELETE = exceptionFilter(
+  "CATEGORY",
+  "DELETE",
+  authGuard(
+    IDValidator<IPropsWithStoreidCategoryidParam>(
+      async (_req: NextRequest, { params: { storeId, categoryId } }: IPropsWithStoreidCategoryidParam) => {
+        const userId = auth().userId as string
 
-  try {
-    const { userId } = auth()
-    if (!userId) {
-      return new NextResponse("Unauthenticated", { status: 401 })
-    }
+        const storeByUserId = await prismadb.store.findFirst({
+          where: {
+            id: storeId,
+            userId,
+          },
+        })
 
-    const category = await prismadb.category.delete({
-      where: { id: categoryId, storeId },
-    })
+        if (!storeByUserId) {
+          return new NextResponse("Unauthorized", { status: 403 })
+        }
 
-    return new NextResponse(JSON.stringify(category), { status: 200 })
-  } catch (error) {
-    console.log("[CATEGORY_DELETE]", error)
-    return new NextResponse("Internal Server ErrorDisplay", { status: 500 })
-  }
-}
+        const category = await prismadb.category.delete({
+          where: { id: categoryId, storeId },
+        })
+
+        return new NextResponse(JSON.stringify(category), { status: 200 })
+      },
+    ),
+  ),
+)
--- app/api/(routes)/[storeId]/categories/route.ts
+++ app/api/(routes)/[storeId]/categories/route.ts
@@ -1,68 +1,59 @@
-import { ObjectId } from "bson"
 import type { IPropsWithStoreidParam } from "@/types/pages-props.interface"
 import prismadb from "@/lib/prismadb"
+import { authGuard } from "@/app/api/lib/auth-guard"
+import { exceptionFilter } from "@/app/api/lib/exception-filter"
+import { IDValidator } from "@/app/api/lib/id-validator"
 import { auth } from "@clerk/nextjs/server"
 import type { NextRequest } from "next/server"
 import { NextResponse } from "next/server"
 
 // Send all categories of a store
-export async function GET(_req: NextRequest, { params: { storeId } }: IPropsWithStoreidParam) {
-  if (!ObjectId.isValid(storeId)) {
-    return new NextResponse("Invalid store id", { status: 400 })
-  }
-
-  try {
+export const GET = exceptionFilter(
+  "CATEGORIES",
+  "GET",
+  IDValidator<IPropsWithStoreidParam>(async (_req: NextRequest, { params: { storeId } }: IPropsWithStoreidParam) => {
     const categories = await prismadb.category.findMany({
       where: { storeId },
     })
 
     return new NextResponse(JSON.stringify(categories), { status: 200 })
-  } catch (error) {
-    console.log("[CATEGORIES_GET]", error)
-    return new NextResponse("Internal Server ErrorDisplay", { status: 500 })
-  }
-}
+  }),
+)
 
 // Create a new category for a store
-export async function POST(req: NextRequest, { params: { storeId } }: IPropsWithStoreidParam) {
-  if (!ObjectId.isValid(storeId)) {
-    return new NextResponse("Invalid store id", { status: 400 })
-  }
+export const POST = exceptionFilter(
+  "",
+  "POST",
+  authGuard(
+    IDValidator<IPropsWithStoreidParam>(async (req: NextRequest, { params: { storeId } }: IPropsWithStoreidParam) => {
+      const userId = auth().userId as string
 
-  try {
-    const { userId } = auth()
-    if (!userId) {
-      return new NextResponse("Unauthenticated", { status: 401 })
-    }
+      const { name, billboardId } = await req.json()
+      if (!name) {
+        return new NextResponse("Name is required", { status: 400 })
+      }
 
-    const { name, billboardId } = await req.json()
-    if (!name) {
-      return new NextResponse("Name is required", { status: 400 })
-    }
+      if (!billboardId) {
+        return new NextResponse("Billboard id is required", { status: 400 })
+      }
 
-    if (!billboardId) {
-      return new NextResponse("Billboard id is required", { status: 400 })
-    }
+      const storeByUserId = await prismadb.store.findFirst({
+        where: { id: storeId, userId },
+      })
 
-    const storeByUserId = await prismadb.store.findFirst({
-      where: { id: storeId, userId },
-    })
+      if (!storeByUserId) {
+        return new NextResponse("Unauthorized", { status: 403 })
+      }
 
-    if (!storeByUserId) {
-      return new NextResponse("Unauthorized", { status: 403 })
-    }
+      const category = await prismadb.category.create({
+        data: {
+          name,
+          billboardId,
+          storeId,
+        },
+      })
 
-    const category = await prismadb.category.create({
-      data: {
-        name,
-        billboardId,
-        storeId,
-      },
-    })
-
-    return new NextResponse(JSON.stringify(category), { status: 201 })
-  } catch (error) {
-    console.log("[CATEGORIES_POST]", error)
-    return new NextResponse("Internal Server ErrorDisplay", { status: 500 })
-  }
-}
+      return new NextResponse(JSON.stringify(category), { status: 201 })
+    }),
+  ),
+)
