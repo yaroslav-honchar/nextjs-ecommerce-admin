8c8ad8e788de7e2c4dc281de9079adafcfb732b3
Yaroslav Honchar <yaroslav.honchar.1@gmail.com>
2024-09-04 23:02:49 +0300

    fix: hex validation

--- app/(dashboard)/[storeId]/(routes)/colors/[colorId]/components/form.schema.ts
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/components/form.schema.ts
@@ -2,7 +2,7 @@ import * as zod from "zod"
 
 export const colorDataSchema = zod.object({
   name: zod.string().min(1),
-  value: zod.string().regex(/^#[0-9A-Fa-f]{6}$/, "Value must be a valid hex color. Example: #FF0000"),
+  value: zod.string().regex(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/, "Value must be a valid hex color. Example: #FF0000"),
 })
 
 export type ColorDataType = zod.infer<typeof colorDataSchema>
